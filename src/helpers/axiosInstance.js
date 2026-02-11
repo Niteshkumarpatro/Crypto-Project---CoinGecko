09d16d275a71b65f5964b3b6676a0aaaffebe371
Nitesh Kumar Patro <niteshkumarpatra@Niteshs-MacBook-Air.local>
2026-02-11 19:46:30 +0530

    Added axios config

---
+++ src/helpers/axiosInstance.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+import { COINGECKO_API_URL } from "./constansts";
+
+
+const axiosInstance =axios.create({
+    baseURL:COINGECKO_API_URL,
+});
+export default axiosInstance;
